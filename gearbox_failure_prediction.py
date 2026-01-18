cd654461a461c6e0249afb8170d0256f4c74702d
siddiquizoya0511-hue <siddiquizoya0511@gmail.com>
2026-01-18 11:21:34 +0530

    Update gearbox_failure_prediction.py

--- gearbox_failure_prediction.py
+++ gearbox_failure_prediction.py
@@ -13,17 +13,17 @@ st.set_page_config(page_title='Gearbox failure Classification', layout="centered
 st.title('Gearbox failure Classification App')
 st.write("Predict the Faiure rate")
 
-Load_%	= st.number_input("Value of Load_%", value=None)
+Load	= st.number_input("Value of Load_%", value=None)
 Temperature_C = st.number_input("Value of Temperature_C,", value=None)
 Vibration_mm_s	= st.number_input("Value of Vibration_mm_s,", value=None)
-Oil_Level_% = st.number_input("Value of Oil_Level_%,", value=None)
+Oil_Level= st.number_input("Value of Oil_Level_%,", value=None)
 
 
 if st.button('Predict'):
-  input_data = np.array([[Load_%,
+  input_data = np.array([[Load,
                           Temperature_C,
                           Vibration_mm_s,
-                          Oil_Level_%
+                          Oil_Level
                           ]])
   prediction = model.predict(input_data)[0]
   if prediction == 0:
